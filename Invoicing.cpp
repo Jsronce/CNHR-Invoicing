f595eea2004f4b356fe8cc7bad1ecc0fa779fcb1
Jsronce <js82@live.missouristate.edu>
2015-05-20 13:06:04 -0500

    Added Sold To Headers

--- Invoicing.cpp
+++ Invoicing.cpp
@@ -29,9 +29,9 @@ error_handler(HPDF_STATUS error_no,
 
 //Used Headers for invoicing
 vector<string> HEADER_LIST{ "Invoice#", "OT" "Man_PO", "Date", "Terms",
-"Sales Person", "Ship Via", "SoldTO", "ShipTO", "Ship-Ad1",
-"Ship-Ad2", "Ship-City", "Ship-ST", "Ship-Zip", "Message1",
-"Message2", "Message3", "Credit" };
+"Sales Person", "Ship Via", "SoldTO", "Sold-Name", "Sold-Ad1", "Sold-City", "Sold-St",
+"Sold-Zip" ,"ShipTO", "Ship-Name" ,"Ship-Ad1", "Ship-Ad2", "Ship-City", "Ship-ST", 
+"Ship-Zip", "Message1", "Message2", "Message3", "Credit", "BOL" };
 
 //Determines whether invoice is sent or not
 set<string> INVOICED_ORDER_TYPES = { "05", "06", "07", "14" ,"08", "15", "71" };
@@ -50,17 +50,18 @@ private:
 	
 
 public:
-	record(vector<string> invoice){
+	record(vector<string> invoice, vector<vector<string>> customers){
 		invoice_number = invoice[0];
 		headers["Invoice#"] = invoice[0];
 		headers["OT"] = invoice[1];
-		headers["Man_PO"] = invoice[2];
+		headers["Man_PO"] = invoice[3];
 		headers["Date"] = invoice[21];
-		headers["Terms"] = invoice[26];
+		headers["Terms"] = invoice[30];
 		headers["Sales Person"] = invoice[38];
-		headers["Ship Via"] = invoice[25];
+		headers["Ship Via"] = invoice[29];
 		headers["SoldTO"] = invoice[31];
-		headers["ShipTO"] = invoice[7];
+		headers["Ship-Name"] = invoice[7];
+		headers["ShipTO"] = invoice[6];
 		headers["Ship-Ad1"] = invoice[33];
 		headers["Ship-Ad2"] = invoice[34];
 		headers["Ship-City"] = invoice[35];
@@ -70,6 +71,17 @@ public:
 		headers["Message2"] = invoice[24];
 		headers["Message3"] = invoice[25];
 		headers["Credit"] = invoice[9];
+
+		for (int i = 0; i < customers.size(); i++){
+			if (customers[i][0] == headers["SoldTO"]){
+				headers["Sold-Name"] = customers[i][1];
+				headers["Sold-Ad1"] = customers[i][3];
+				headers["Sold-City"] = customers[i][4];
+				headers["Sold-ST"] = customers[i][5];
+				headers["Sold-Zip"] = customers[i][6];
+				break;
+			}
+		}
 
 
 
@@ -130,7 +142,7 @@ public:
 
 //Fucntion to create a table of values from a delimited file
 //Takes File as fname and char Delimiter defaults to '\t'(tab)
-vector<vector<string>> create_table(string fname, char delimeter='\t'){
+vector<vector<string>> create_table(string fname, char delimeter='\t', bool invoice = true){
 	cout << "Importing File";
 	ifstream infile;
 	infile.open(fname, ios::in);
@@ -154,7 +166,7 @@ vector<vector<string>> create_table(string fname, char delimeter='\t'){
 			line.push_back(record);
 			record = "";
 			if (token == '\n'){
-				if (INVOICED_ORDER_TYPES.count(line[1]) != 0 || MOVE_TO_AP_TYPES.count(line[1]) != 0)
+				if (INVOICED_ORDER_TYPES.count(line[1]) != 0 || MOVE_TO_AP_TYPES.count(line[1]) != 0 || !invoice)
 					data.push_back(line);
 				line.clear();
 			}
@@ -164,6 +176,18 @@ vector<vector<string>> create_table(string fname, char delimeter='\t'){
 	} while (!infile.eof());
 	infile.close();
 	return data;
+}
+
+vector<HPDF_REAL> setText(HPDF_Page page, HPDF_REAL x, HPDF_REAL y, vector<HPDF_REAL> pos){
+	vector<HPDF_REAL> Newpos = pos;
+	HPDF_REAL xPos = pos[0];
+	HPDF_REAL yPos = pos[1];
+	HPDF_Page_MoveTextPos(page, x, y);
+	xPos = xPos + x;
+	yPos = yPos - y;
+	Newpos[0] = xPos;
+	Newpos[1] = yPos;
+	return Newpos;
 }
 
 int create_PDF(record* invoice, vector<vector<string>> customers){
@@ -178,23 +202,29 @@ int create_PDF(record* invoice, vector<vector<string>> customers){
 	try{
 		const char* image_path = "\\\\psserver1\\CNHR_Depts\\Accounting\\Shipment Reports\\Processing\\CNHI Reman Logo Compressed.jpg";
 		HPDF_Image logo = HPDF_LoadJpegImageFromFile(pdf, image_path);
+
+
+		//Future Create Page Method
 		HPDF_Page page = HPDF_AddPage(pdf);
 		HPDF_REAL height;
+		
+
 		height = HPDF_Page_GetHeight(page);
+		HPDF_REAL heightMargin = height*.95;
 		HPDF_REAL width;
 		width = HPDF_Page_GetWidth(page);
-		HPDF_Page_DrawImage(page, logo, width*.1, height*.9 -56, 160, 56);
-		vector<HPDF_REAL> pos;
-		pos[0] = 0;
-		pos[1] = 0;
+		HPDF_REAL widthMargin = width*.95;
+		HPDF_Page_DrawImage(page, logo, width*.05, height*.95 -56, 160, 56);
+		vector<HPDF_REAL> pos = { 0, 0 };
+
 		
 		HPDF_Page_BeginText(page);
-		pos = setText(page, width*.1 + 160 + 10, height*.9 - 4, pos);
+		pos = setText(page, width*.05 + 160 + 20, height*.95 -16, pos);
 
 
-		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", NULL), 12);
-		HPDF_Page_ShowText(page, "Please Remit to:");
-		pos = setText(page, 0, -12, pos);
+		//HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", NULL), 12);
+		//HPDF_Page_ShowText(page, "Please Remit to:");
+		//pos = setText(page, 0, -12, pos);
 
 		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica-Bold", NULL), 16);
 		HPDF_Page_ShowText(page, "CNH Reman, LLC");
@@ -206,29 +236,50 @@ int create_PDF(record* invoice, vector<vector<string>> customers){
 
 		HPDF_Page_ShowText(page, "Springfield MO, 65807");
 
-		
+
 		//BELOW HERE NEEDS TO BE EDITED FOR Re-arrangement
-		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", NULL), 24);
+		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica-Bold", NULL), 16);
 		const char* title = "Invoice:";
 		if (invoice->get_header("Credit") == "C")
 			title = "Credit:";
 		string strstuf = string(title) + " " + invoice->get_header("Credit") + invoice->get_header("Invoice#");
 		const char* stuff = strstuf.c_str();
 		HPDF_REAL space = HPDF_Page_TextWidth(page, title);
-		pos = setText(page, width*.9 - pos[0] - HPDF_Page_TextWidth(page, title), 40, pos);
+
+		pos = setText(page, width*.95 - pos[0] - HPDF_Page_TextWidth(page, title), 28, pos);
 	
-		HPDF_Page_MoveTextPos(page, (width - HPDF_Page_TextWidth(page, stuff)) * 4 / 9, height*.95);
-		HPDF_Page_ShowText(page, stuff);
+		
+		HPDF_Page_ShowText(page, title);
+
 		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", NULL), 12);
-		HPDF_Page_MoveTextPos(page, 0, -24);
-		HPDF_Page_ShowText(page, ("Date: " + invoice->get_header("Date")).c_str());
+		string tempStr = "#" + invoice->get_header("Credit") + invoice->get_header("Invoice#");
+		const char* invNum = tempStr.c_str();
+		pos = setText(page, width*.95 - pos[0] - HPDF_Page_TextWidth(page, invNum), -16, pos);
+		HPDF_Page_ShowText(page, invNum);
+
+		tempStr = "Date: " + invoice->get_header("Date");
+		const char* date = tempStr.c_str();
+		pos = setText(page, width*.95 - pos[0] - HPDF_Page_TextWidth(page, date), -12, pos);
+		HPDF_Page_ShowText(page, date);
 
 		HPDF_Page_EndText(page);
-		HPDF_Page_SetLineWidth(page, 1);
-		HPDF_Page_Stroke(page);
+
 		
-		
-		
+		//End Create Page
+
+		HPDF_Page_BeginText(page);
+		pos = { 0, 0 };
+		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", NULL), 10);
+		pos = setText(page, widthMargin, heightMargin - 86, pos);
+		HPDF_Page_ShowText(page, "Sold to");
+		pos = setText(page, 10, -10, pos);
+		HPDF_Page_SetFontAndSize(page, HPDF_GetFont(pdf, "Helvetica", NULL), 12);
+		HPDF_Page_ShowText(page, invoice->get_header("SoldTO").c_str());
+		HPDF_Page_ShowText(page, invoice->get_header("SoldTO").c_str());
+		HPDF_Page_ShowText(page, invoice->get_header("SoldTO").c_str());
+		HPDF_Page_ShowText(page, invoice->get_header("SoldTO").c_str());
+
+
 		
 		
 		
@@ -251,17 +302,7 @@ int create_PDF(record* invoice, vector<vector<string>> customers){
 }
 
 
-vector<HPDF_REAL> setText(HPDF_Page page ,HPDF_REAL x, HPDF_REAL y, vector<HPDF_REAL> pos){
-	vector<HPDF_REAL> Newpos = pos;
-	HPDF_REAL xPos = pos[0];
-	HPDF_REAL yPos = pos[1];
-	HPDF_Page_MoveTextPos(page, x, y);
-	xPos = xPos + x;
-	yPos = yPos - y;
-	Newpos[0] = xPos;
-	Newpos[1] = yPos;
-	return Newpos;
-}
+
 
 
 int main(int argc, char **argv)
@@ -269,7 +310,7 @@ int main(int argc, char **argv)
 
 	string path = "\\\\psserver1\\CNHR_Depts\\Accounting\\Shipment Reports\\Processing";
 	vector<vector<string>> invoices = create_table(path + "\\invoice.csv", '\t');
-	vector<vector<string>> customers = create_table(path + "\\customer.csv", ',');
+	vector<vector<string>> customers = create_table(path + "\\customer.csv", ',', false);
 	
 	string previous;
 	record* invoice = NULL;
@@ -297,7 +338,7 @@ int main(int argc, char **argv)
 				delete invoice;
 			}
 			//create new record
-			invoice = new record(invoices[i]);
+			invoice = new record(invoices[i], customers);
 		}
 		previous = *current;
 		delete current;
